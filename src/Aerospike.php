f170b017b8021778d501da639a92e0d121f23363
Serghei Iakovlev <sadhooklay@gmail.com>
2015-12-04 00:25:57 +0200

    Prepared constants, added doc for Aerospike::increment

--- src/Aerospike.php
+++ src/Aerospike.php
@@ -2,43 +2,197 @@
 
 final class Aerospike
 {
+    /**
+     * Batch-direct or batch-index protocol. default: 0
+     * @type int
+     */
     const USE_BATCH_DIRECT = 17;
+
+    /**
+     * UDF Lua type
+     * @type int
+     */
     const UDF_TYPE_LUA = 0;
+
+    /**
+     * @type int
+     */
     const SERIALIZER_USER = 3;
+
+    /**
+     * @type int
+     */
     const SERIALIZER_PHP = 1;
+
+    /**
+     * @type int
+     */
     const SERIALIZER_NONE = 0;
+
+    /**
+     * @type int
+     */
     const SERIALIZER_JSON = 2;
+
+    /**
+     * @type int
+     */
     const SCAN_STATUS_UNDEF = 0;
+
+    /**
+     * @type int
+     */
     const SCAN_STATUS_INPROGRESS = 1;
+
+    /**
+     * @type int
+     */
     const SCAN_STATUS_COMPLETED = 3;
+
+    /**
+     * @type int
+     */
     const SCAN_STATUS_ABORTED = 2;
+
+    /**
+     * @type int
+     */
     const SCAN_PRORITY_LOW = 1;
+
+    /**
+     * @type int
+     */
     const SCAN_PRIORITY_MEDIUM = 2;
+
+    /**
+     * @type int
+     */
     const SCAN_PRIORITY_HIGH = 3;
+
+    /**
+     * @type int
+     */
     const SCAN_PRIORITY_AUTO = 0;
+
+    /**
+     * @type int
+     */
     const PRIV_USER_ADMIN = 0;
+
+    /**
+     * @type int
+     */
     const PRIV_SYS_ADMIN = 1;
+
+    /**
+     * @type int
+     */
     const PRIV_READ_WRITE_UDF = 12;
+
+    /**
+     * @type int
+     */
     const PRIV_READ_WRITE = 11;
+
+    /**
+     * @type int
+     */
     const PRIV_READ = 10;
+
+    /**
+     * @type int
+     */
     const PRIV_DATA_ADMIN = 2;
+
+    /**
+     * @type int
+     */
     const POLICY_RETRY_ONCE = 1;
+
+    /**
+     * @type int
+     */
     const POLICY_RETRY_NONE = 0;
+
+    /**
+     * @type int
+     */
     const POLICY_REPLICA_MASTER = 0;
+
+    /**
+     * @type int
+     */
     const POLICY_REPLICA_ANY = 1;
+
+    /**
+     * @type int
+     */
     const POLICY_KEY_SEND = 1;
+
+    /**
+     * Hashes (ns,set,key) data into a unique record ID (default)
+     * @type int
+     */
     const POLICY_KEY_DIGEST = 0;
+
+    /**
+     * @type int
+     */
     const POLICY_GEN_IGNORE = 0;
+
+    /**
+     * @type int
+     */
     const POLICY_GEN_GT = 2;
+
+    /**
+     * @type int
+     */
     const POLICY_GEN_EQ = 1;
+
+    /**
+     * @type int
+     */
     const POLICY_EXISTS_UPDATE = 2;
+
+    /**
+     * @type int
+     */
     const POLICY_EXISTS_REPLACE = 3;
+
+    /**
+     * @type int
+     */
     const POLICY_EXISTS_IGNORE = 0;
+
+    /**
+     * @type int
+     */
     const POLICY_EXISTS_CREATE_OR_REPLACE = 4;
+
+    /**
+     * @type int
+     */
     const POLICY_EXISTS_CREATE = 1;
+
+    /**
+     * @type int
+     */
     const POLICY_CONSISTENCY_ONE = 0;
+
+    /**
+     * @type int
+     */
     const POLICY_CONSISTENCY_ALL = 1;
+
+    /**
+     * @type int
+     */
     const POLICY_COMMIT_LEVEL_MASTER = 1;
+
+    /**
+     * @type int
+     */
     const POLICY_COMMIT_LEVEL_ALL = 0;
 
     // Query Predicate Operators
@@ -47,99 +201,474 @@ final class Aerospike
     const OP_CONTAINS = 'CONTAINS';
     const OP_BETWEEN = 'BETWEEN';
 
+    /**
+     * @type int
+     */
     const OPT_WRITE_TIMEOUT = 3;
+
+    /**
+     * @type int
+     */
     const OPT_TTL = 16;
+
+    /**
+     * @type int
+     */
     const OPT_SERIALIZER = 6;
+
+    /**
+     * @type int
+     */
     const OPT_SCAN_PRIORITY = 7;
+
+    /**
+     * @type int
+     */
     const OPT_SCAN_PERCENTAGE = 8;
+
+    /**
+     * @type int
+     */
     const OPT_SCAN_NOBINS = 10;
+
+    /**
+     * @type int
+     */
     const OPT_SCAN_CONCURRENTLY = 9;
+
+    /**
+     * @type int
+     */
     const OPT_READ_TIMEOUT = 2;
+
+    /**
+     * @type int
+     */
     const OPT_POLICY_RETRY = 4;
+
+    /**
+     * @type int
+     */
     const OPT_POLICY_REPLICA = 13;
+
+    /**
+     * @type int
+     */
     const OPT_POLICY_KEY = 11;
+
+    /**
+     * @type int
+     */
     const OPT_POLICY_GEN = 12;
+
+    /**
+     * @type int
+     */
     const OPT_POLICY_EXISTS = 5;
+
+    /**
+     * @type int
+     */
     const OPT_POLICY_CONSISTENCY = 14;
+
+    /**
+     * @type int
+     */
     const OPT_POLICY_COMMIT_LEVEL = 15;
+
+    /**
+     * @type int
+     */
     const OPT_CONNECT_TIMEOUT = 1;
+
+    /**
+     * @type int
+     */
     const OPERATOR_WRITE = 2;
+
+    /**
+     * @type int
+     */
     const OPERATOR_TOUCH = 11;
+
+    /**
+     * @type int
+     */
     const OPERATOR_READ = 1;
+
+    /**
+     * @type int
+     */
     const OPERATOR_PREPEND = 10;
+
+    /**
+     * @type int
+     */
     const OPERATOR_INCR = 5;
+
+    /**
+     * @type int
+     */
     const OPERATOR_APPEND = 9;
+
+    /**
+     * @type int
+     */
     const OK = 0;
+
+    /**
+     * @type int
+     */
     const LOG_LEVEL_WARN = 4;
+
+    /**
+     * @type int
+     */
     const LOG_LEVEL_TRACE = 1;
+
+    /**
+     * @type int
+     */
     const LOG_LEVEL_OFF = 6;
+
+    /**
+     * @type int
+     */
     const LOG_LEVEL_INFO = 3;
+
+    /**
+     * @type int
+     */
     const LOG_LEVEL_ERROR = 5;
+
+    /**
+     * @type int
+     */
     const LOG_LEVEL_DEBUG = 2;
+
+    /**
+     * @type int
+     */
     const INDEX_TYPE_STRING = 0;
+
+    /**
+     * @type int
+     */
     const INDEX_TYPE_MAPVALUES = 3;
+
+    /**
+     * @type int
+     */
     const INDEX_TYPE_MAPKEYS = 2;
+
+    /**
+     * @type int
+     */
     const INDEX_TYPE_LIST = 1;
+
+    /**
+     * @type int
+     */
     const INDEX_TYPE_INTEGER = 1;
+
+    /**
+     * @type int
+     */
     const INDEX_TYPE_DEFAULT = 0;
+
+    /**
+     * @type int
+     */
     const INDEX_STRING = 0;
+
+    /**
+     * @type int
+     */
     const INDEX_NUMERIC = 1;
+
+    /**
+     * @type int
+     */
     const ERR_USER_ALREADY_EXISTS = 61;
+
+    /**
+     * @type int
+     */
     const ERR_UNSUPPORTED_FEATURE = 16;
+
+    /**
+     * @type int
+     */
     const ERR_UDF_NOT_FOUND = 1301;
+
+    /**
+     * @type int
+     */
     const ERR_UDF = 100;
+
+    /**
+     * @type int
+     */
     const ERR_TIMEOUT = 9;
+
+    /**
+     * @type int
+     */
     const ERR_SERVER_FULL = 8;
+
+    /**
+     * @type int
+     */
     const ERR_SERVER = 1;
+
+    /**
+     * @type int
+     */
     const ERR_SECURITY_SCHEME_NOT_SUPPORTED = 53;
+
+    /**
+     * @type int
+     */
     const ERR_SECURITY_NOT_SUPPORTED = 51;
+
+    /**
+     * @type int
+     */
     const ERR_SECURITY_NOT_ENABLED = 52;
+
+    /**
+     * @type int
+     */
     const ERR_SCAN_ABORTED = 15;
+
+    /**
+     * @type int
+     */
     const ERR_ROLE_VIOLATION = 81;
+
+    /**
+     * @type int
+     */
     const ERR_ROLE_ALREADY_EXISTS = 71;
+
+    /**
+     * @type int
+     */
     const ERR_REQUEST_INVALID = 4;
+
+    /**
+     * @type int
+     */
     const ERR_RECORD_TOO_BIG = 13;
+
+    /**
+     * @type int
+     */
     const ERR_RECORD_NOT_FOUND = 2;
+
+    /**
+     * @type int
+     */
     const ERR_RECORD_KEY_MISMATCH = 19;
+
+    /**
+     * @type int
+     */
     const ERR_RECORD_GENERATION = 3;
+
+    /**
+     * @type int
+     */
     const ERR_RECORD_EXISTS = 5;
+
+    /**
+     * @type int
+     */
     const ERR_RECORD_BUSY = 14;
+
+    /**
+     * @type int
+     */
     const ERR_QUERY_TIMEOUT = 212;
+
+    /**
+     * @type int
+     */
     const ERR_QUERY_QUEUE_FULL = 211;
+
+    /**
+     * @type int
+     */
     const ERR_QUERY_END = 50;
+
+    /**
+     * @type int
+     */
     const ERR_QUERY_ABORTED = 210;
+
+    /**
+     * @type int
+     */
     const ERR_QUERY = 213;
+
+    /**
+     * @type int
+     */
     const ERR_PARAM = -2;
+
+    /**
+     * @type int
+     */
     const ERR_NO_XDR = 10;
+
+    /**
+     * @type int
+     */
     const ERR_NOT_AUTHENTICATED = 80;
+
+    /**
+     * @type int
+     */
     const ERR_NAMESPACE_NOT_FOUND = 20;
+
+    /**
+     * @type int
+     */
     const ERR_LUA_FILE_NOT_FOUND = 1302;
+
+    /**
+     * @type int
+     */
     const ERR_LARGE_ITEM_NOT_FOUND = 125;
+
+    /**
+     * @type int
+     */
     const ERR_INVALID_USER = 60;
+
+    /**
+     * @type int
+     */
     const ERR_INVALID_ROLE = 70;
+
+    /**
+     * @type int
+     */
     const ERR_INVALID_PRIVILEGE = 72;
+
+    /**
+     * @type int
+     */
     const ERR_INVALID_PASSWORD = 62;
+
+    /**
+     * @type int
+     */
     const ERR_INVALID_HOST = -4;
+
+    /**
+     * @type int
+     */
     const ERR_INVALID_FIELD = 55;
+
+    /**
+     * @type int
+     */
     const ERR_INVALID_CREDENTIAL = 65;
+
+    /**
+     * @type int
+     */
     const ERR_INVALID_COMMAND = 54;
+
+    /**
+     * @type int
+     */
     const ERR_INDEX_OOM = 202;
+
+    /**
+     * @type int
+     */
     const ERR_INDEX_NOT_READABLE = 203;
+
+    /**
+     * @type int
+     */
     const ERR_INDEX_NOT_FOUND = 201;
+
+    /**
+     * @type int
+     */
     const ERR_INDEX_NAME_MAXLEN = 205;
+
+    /**
+     * @type int
+     */
     const ERR_INDEX_MAXCOUNT = 206;
+
+    /**
+     * @type int
+     */
     const ERR_INDEX_FOUND = 200;
+
+    /**
+     * @type int
+     */
     const ERR_INDEX = 204;
+
+    /**
+     * @type int
+     */
     const ERR_ILLEGAL_STATE = 56;
+
+    /**
+     * @type int
+     */
     const ERR_FORBIDDEN_PASSWORD = 64;
+
+    /**
+     * @type int
+     */
     const ERR_EXPIRED_PASSWORD = 63;
+
+    /**
+     * @type int
+     */
     const ERR_DEVICE_OVERLOAD = 18;
+
+    /**
+     * @type int
+     */
     const ERR_CLUSTER_CHANGE = 7;
+
+    /**
+     * @type int
+     */
     const ERR_CLUSTER = 11;
+
+    /**
+     * @type int
+     */
     const ERR_CLIENT = -1;
+
+    /**
+     * @type int
+     */
     const ERR_BIN_NOT_FOUND = 17;
+
+    /**
+     * @type int
+     */
     const ERR_BIN_NAME = 21;
+
+    /**
+     * @type int
+     */
     const ERR_BIN_INCOMPATIBLE_TYPE = 12;
+
+    /**
+     * @type int
+     */
     const ERR_BIN_EXISTS = 6;
 
     /**
@@ -524,10 +1053,28 @@ final class Aerospike
     }
 
     /**
-     * @param array  $key
-     * @param string $bin
-     * @param int    $offset
-     * @param array  $options
+     * Increments a numeric value in a bin.
+     *
+     * <code>
+     * $db = new Aerospike($config);
+     *
+     * $key = $db->initKey('test', 'users', 1234);
+     * $options = [Aerospike::OPT_TTL => 7200];
+     *
+     * $status = $db->increment($key, 'pto', -4, $options);
+     *
+     * var_dump($status);
+     * </code>
+     *
+     * @param array  $key     The key under which the record can be found.
+     *                        An array with keys ['ns','set','key'] or ['ns','set','digest'].
+     * @param string $bin     The name of the bin in which we have a numeric value.
+     * @param int    $offset  The integer by which to increment the value in the bin.
+     * @param array  $options Options including:
+     *                        Aerospike::OPT_WRITE_TIMEOUT, Aerospike::OPT_TTL
+     *                        Aerospike::OPT_POLICY_RETRY, Aerospike::OPT_POLICY_KEY,
+     *                        Aerospike::OPT_POLICY_GEN, Aerospike::OPT_POLICY_REPLICA,
+     *                        Aerospike::OPT_POLICY_CONSISTENCY, Aerospike::OPT_POLICY_COMMIT_LEVEL [Optional]
      *
      * @return int
      */
@@ -732,7 +1279,7 @@ final class Aerospike
      * $status = $db->scan('test', 'users', function ($record) use (&$processed) {
      *     if (!is_null($record['bins']['email'])) echo $record['bins']['email']."\n";
      *     if ($processed++ > 19) return false; // halt the stream by returning a false
-     * }, array("email"), $options);
+     * }, ['email'], $options);
      *
      * var_dump($status, $processed);
      * </code>
